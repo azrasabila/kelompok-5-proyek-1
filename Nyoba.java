a058aead24bc6cd07970d7e9b15128afc41c5828
Azra Sabila <36650527+azrasabila@users.noreply.github.com>
2018-02-20 09:04:18 +0700

    nyobain upload file

---
+++ Nyoba.java
@@ -0,0 +1,18 @@
+public class Nyoba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double radius;
+        double area;
+        
+        radius = 20;
+        
+        area = radius * radius * 3.14159;
+        
+        System.out.println(area);
+    }
+    
+}
