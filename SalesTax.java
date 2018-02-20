29e4ae31c9010bc885d006cc8c94d3979ab225bd
Alifia Syalsabila <34833963+alifiasyalsa@users.noreply.github.com>
2018-02-20 09:41:48 +0700

    Update SalesTax.java

--- SalesTax.java
+++ SalesTax.java
@@ -9,6 +9,6 @@ public class SalesTax {
 
 
 		double tax = purchaseAmount * 0.06;
-		System.out.println("Sales tax is " + (int)(tax*100) / 100.0);
+		System.out.println("Sales tax is " + (int)(tax*100) / 100.0 + "$$");
 	}
 }
